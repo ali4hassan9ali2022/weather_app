4826152d78e37c9898d029dcbfa1d64809999a94
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-07 16:18:51 +0200

    create app bar to home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/widgets/no_weather_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static String id = "HomeView";
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather app"),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+      ),
+      body: const NoWeatherBody(),
+    );
   }
 }
