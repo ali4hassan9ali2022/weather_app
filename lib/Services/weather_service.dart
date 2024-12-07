ec88a3d9b43cc1c81d394674b9c0c432c0b7dd16
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2024-12-07 16:36:41 +0200

    create weather service

---
+++ lib/Services/weather_service.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+  final String baseUrl = "https://api.weatherapi.com/v1";
+  final String apiKey = "c9510e43dd2e48eba0c13610240508";
+  const WeatherService(this.dio);
+  Future<WeatherModel> getWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get("$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1");
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errMassage = e.response?.data["error"]["message"] ??
+          "oops there was an error, try later!";
+      throw Exception(errMassage);
+    } catch (e) {
+      throw Exception("oops there was an error, try later!");
+    }
+  }
+}
